b96b76e6c4de2c0db6ac3717612e534fee731fe7
yagmurrizvi <50454736+yagmurrizvi@users.noreply.github.com>
2022-04-03 11:20:27 +0200

    Added the Github repository URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Yagmur Rizvi and is{" "}
+        <a
+          href="https://github.com/yagmurrizvi/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
