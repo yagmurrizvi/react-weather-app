28834c6a397da899b7fda4140a56bbe9e0cecafb
yagmurrizvi <50454736+yagmurrizvi@users.noreply.github.com>
2022-04-04 23:51:47 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/yagmurrizvi/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on Github
           </a>
