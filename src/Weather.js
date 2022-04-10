f1a5963575ef9400f700271f91cc2cd8d4c9fb34
yagmurrizvi <50454736+yagmurrizvi@users.noreply.github.com>
2022-04-10 10:45:10 +0200

    rename weatherInfo

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import WeatherInfo from "./WeatherInfo";
+import WeatherInfo from "./Weatherinfo";
 import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
 
